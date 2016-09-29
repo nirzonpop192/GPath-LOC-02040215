9b93df3bb58ce6cf117c3d773e402c8e35441289
Nirzon Pop <nirzon192@gmail.com>
2016-09-29 16:33:31 +0600

    Response Page Ques Load

--- app/src/main/java/com/siddiquinoor/restclient/activity/sub_activity/dynamic_table/DT_QuestionActivity.java
+++ app/src/main/java/com/siddiquinoor/restclient/activity/sub_activity/dynamic_table/DT_QuestionActivity.java
@@ -3,7 +3,9 @@ package com.siddiquinoor.restclient.activity.sub_activity.dynamic_table;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.siddiquinoor.restclient.R;
@@ -11,6 +13,7 @@ import com.siddiquinoor.restclient.fragments.BaseActivity;
 import com.siddiquinoor.restclient.manager.SQLiteHandler;
 import com.siddiquinoor.restclient.utils.KEY;
 import com.siddiquinoor.restclient.views.adapters.DynamicDataIndexDataModel;
+import com.siddiquinoor.restclient.views.adapters.DynamicTableQuesDataModel;
 
 public class DT_QuestionActivity extends BaseActivity {
 
@@ -19,39 +22,107 @@ public class DT_QuestionActivity extends BaseActivity {
     private DynamicDataIndexDataModel dyIndex;
     private int totalQuestion;
     private TextView tv_DtQuestion;
+    private Button btnNextQues;
+
+    private Button btnPreviousQus;
+    int mQusIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dt__qustion);
         inti();
+        DynamicTableQuesDataModel qus = fistQuestion(dyIndex.getDtBasicCode());
+        displayQuestion(qus.getqText());
+
+        setListener();
 
     }
 
-    private void inti() {
 
+    private void setListener() {
+        btnNextQues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ++mQusIndex;
+                /**
+                 * to check does index exceed the max value
+                 */
+                if (mQusIndex < totalQuestion) {
+                    DynamicTableQuesDataModel nextQus = loadNextQuestion(dyIndex.getDtBasicCode(), mQusIndex);
+
+                    displayQuestion(nextQus.getqText());
+                }else if (mQusIndex==totalQuestion){
+                    mQusIndex=totalQuestion-1;
+                    Log.d("MOR","mQusIndex: "+mQusIndex);
+                }
+
+
+
+            }
+        });
+        btnPreviousQus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /**
+                 *  TODO: 9/29/2016  save method & update method
+                 */
+
+                --mQusIndex;
+                /**
+                 * to check does index exceed the max value
+                 */
+                if (mQusIndex >= 0) {
+                    DynamicTableQuesDataModel nextQus = loadPreviousQuestion(dyIndex.getDtBasicCode(), mQusIndex);
+
+                    displayQuestion(nextQus.getqText());
+                } else if (mQusIndex < 0) {
+                    mQusIndex = 0;
+                    Log.d("MOR","mQusIndex: "+mQusIndex);
+                }
+
+            }
+        });
+    }
+
+    private void displayQuestion(String qus) {
+        tv_DtQuestion.setText(qus);
+    }
+
+    private void inti() {
+        viewReference();
         sql = new SQLiteHandler(mContext);
         Intent intent = getIntent();
         dyIndex = intent.getParcelableExtra(KEY.DYNAMIC_INDEX_DATA_OBJECT_KEY);
         totalQuestion = intent.getIntExtra(KEY.DYNAMIC_T_QUES_SIZE, 0);
+        mQusIndex = 0;
 
     }
 
-    private void viewRefernce() {
+    private void viewReference() {
         tv_DtQuestion = (TextView) findViewById(R.id.tv_DtQuestion);
+        btnNextQues = (Button) findViewById(R.id.btnHomeFooter);
+        btnPreviousQus = (Button) findViewById(R.id.btnRegisterFooter);
+        btnNextQues.setText("Next");
+        btnPreviousQus.setText("Previous");
     }
 
-    private void fristQuestion() {
+    private DynamicTableQuesDataModel fistQuestion(final String dtBasic) {
+        return loadQuestion(dtBasic, 0);
     }
 
-    private void loadQuestion(final String dtBasic,final int qusIndex) {
-        sql.getSingleDynamicQuestion(dtBasic,qusIndex);
+    private DynamicTableQuesDataModel loadQuestion(final String dtBasic, final int qusIndex) {
+        return sql.getSingleDynamicQuestion(dtBasic, qusIndex);
     }
 
-    private void loadNextQuestion() {
+    private DynamicTableQuesDataModel loadNextQuestion(final String dtBasic, final int qusIndex) {
+
+        return loadQuestion(dtBasic, qusIndex);
     }
 
-    private void loadPreviousQuestion() {
+    private DynamicTableQuesDataModel loadPreviousQuestion(final String dtBasic, final int qusIndex) {
+
+        return loadQuestion(dtBasic, qusIndex);
     }
 
 }
